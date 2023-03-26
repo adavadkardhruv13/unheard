9939059650eba9876589ab9464826b72081d33cc
Dhruv Adavadkar <adavadkardhruv@gmail.com>
2023-03-26 12:40:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { LandingPage } from "./components";
+
+
+const App = () => {
+  return ( <div className="w.screen h-screen flex flex-col">
+    <LandingPage />
+  </div>)
+}
+
+export default App;
